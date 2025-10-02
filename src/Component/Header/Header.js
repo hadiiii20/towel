ffadee9b8e206a3eed68aec04b1d67b0f3c38cc0
hadiiii20 <hadinaderkhani@gmail.com>
2025-10-01 16:40:06 -0800

    Edit Header and remover Swiper from Header

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,20 +2,52 @@ import React from "react";
 import NavbarBlue from "../NavbarBlue/NavbarBlue";
 import "./Header.css";
 import { Link } from "react-router-dom";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { EffectCards } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/effect-cards";
+// import { Autoplay } from "swiper/modules";
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCards } from "swiper/modules";
+
+// Import Swiper styles
 import "swiper/css";
-import "swiper/css/effect-cards";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
 import { Autoplay } from "swiper/modules";
+import { EffectFlip, Pagination, Navigation } from "swiper/modules";
 
 export default function Header() {
     return (
         <div className="container">
             <NavbarBlue />
             <section className="section">
-                <div className="img-men">
+                <Swiper
+                    effect={"flip"}
+                    grabCursor={true}
+                    pagination={true}
+                    navigation={true}
+                    autoplay={{
+                        delay: 2800,
+                        disableOnInteraction: false,
+                    }}
+                    modules={[EffectFlip, Pagination, Navigation, Autoplay]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide className="img-men">
+                        <img src="./images/younas1.png" alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide className="img-men">
+                        <img src="./images/jagvar1.png" alt="" />
+                    </SwiperSlide>
+                </Swiper>
+
+                {/* <div className="img-men">
                     <img src="./images/younas1.png" alt="" />
-                </div>
+                </div> */}
                 <div className="title-wrapper">
                     <h1 className="titleh1">امروز در پودایران</h1>
                     <h3 className="titleh3">کالکشنی زیبا از جدیدترین حوله ها</h3>
@@ -27,19 +59,43 @@ export default function Header() {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            class="bi bi-chevron-left mybtn-svg"
+                            className="bi bi-chevron-double-left mybtn-svg"
                             viewBox="0 0 16 16"
                         >
                             <path
-                                fill-rule="evenodd"
-                                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+                                fillRule="evenodd"
+                                d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+                            />
+                            <path
+                                fillRule="evenodd"
+                                d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
                             />
                         </svg>
                     </Link>
                 </div>
-                <div className="img-woman">
+                <Swiper
+                    effect={"flip"}
+                    grabCursor={true}
+                    pagination={true}
+                    navigation={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    modules={[EffectFlip, Pagination, Navigation, Autoplay]}
+                    className="mySwiper swiper-woman"
+                >
+                    <SwiperSlide className="img-woman">
+                        <img src="./images/paradais.png" alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide className="img-woman">
+                        <img src="./images/palangi1.png" alt="" />
+                    </SwiperSlide>
+                </Swiper>
+
+                {/* <div className="img-woman">
                     <img src="./images/paradais.png" alt="" />
-                </div>
+                </div> */}
                 {/* <Swiper
                     effect={"cards"}
                     grabCursor={true}
@@ -70,6 +126,44 @@ export default function Header() {
                     <img src="./images/mens.png" alt="" />
                 </div> */}
             </section>
+            <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+            </div>
         </div>
     );
 }
--- src/Component/NavbarBlue/NavbarBlue.js
+++ src/Component/NavbarBlue/NavbarBlue.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavbarBlue.css";
 
 export default function NavbarBlue() {
+    const [logoDisplay, setLogoDisplay] = useState(1);
+    const [logoWidth, setLogoWidth] = useState("10rem");
+    const [logoBottom, setLogoBottom] = useState("-5.5rem");
+
+    window.onscroll = () => {
+        let maxScroll = window.pageYOffset;
+        if (maxScroll > 70) {
+            setLogoDisplay(0.7);
+            setLogoWidth("6.7rem");
+            setLogoBottom("0.9rem");
+            console.log(maxScroll);
+        } else {
+            setLogoDisplay(1);
+            setLogoWidth("10rem");
+            setLogoBottom("-5.5rem");
+        }
+    };
     return (
         <nav className="nav">
             <Link className="nav-logo">
-                <img src="./images/logo.png" alt="" style={{ width: "10rem" }} />
+                <img
+                    src="./images/logo.png"
+                    alt=""
+                    style={{ width: `${logoWidth}`, opacity: `${logoDisplay} `, bottom: `${logoBottom}` }}
+                />
             </Link>
             <ul className="menu">
                 <li className="menu-item menu-item__active">
@@ -30,7 +51,7 @@ export default function NavbarBlue() {
                         width="22"
                         height="22"
                         fill="currentColor"
-                        class="bi bi-person-fill"
+                        className="bi bi-person-fill"
                         viewBox="0 0 16 16"
                     >
                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
@@ -42,7 +63,7 @@ export default function NavbarBlue() {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-basket-fill"
+                        className="bi bi-basket-fill"
                         viewBox="0 0 16 16"
                     >
                         <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z" />
